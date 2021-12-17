3ce160ed25c824a3b20fe4b9ad123493962837ba
cabralcarol <ana.melo@healthgotech.com>
2021-12-17 17:19:30 -0300

    Voltando ao estado inicial

--- main.c
+++ main.c
@@ -1,16 +1,3 @@
 #include <stdio.h>
-<<<<<<< HEAD
-#include <spi.h>
-#include <uart.h>
-
-
-void main() {
-
-    config_spi();
-
-    write_uart();
-}
-=======
 
 void main() {}
->>>>>>> parent of 2bf1da6 (Added spi)
