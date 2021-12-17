d35857e8f748b9a134ddbe8b8dfd916cd161e433
Felipe Alcantara <lipelima0327@gmail.com>
2021-12-17 17:41:11 -0300

    Adding Hello World and UART print
    - printf of Hello World
    - UART print loop

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
+#include <uart.h>
 
-void main() {}
+#define COUNT 1000
+void main() {
+
+    printf("Hello World!!");
+    for (size_t i = 0; i < COUNT; i++)
+    {
+        printUART(i);
+    }
+    
+}
